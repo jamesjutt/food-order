b47470a72e8081a7efdf19c439991ee1a7f8bfa0
jamesjutt <jamesrjutt@gmail.com>
2023-10-23 17:14:37 -0400

    remove fragment

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,12 +1,10 @@
-import { Fragment } from "react";
-
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <Fragment>
+    <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton />
@@ -14,7 +12,7 @@ const Header = () => {
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
-    </Fragment>
+    </>
   );
 };
 

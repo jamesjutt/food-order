a8ce5fa0044d697c1ad3b34867261e7e66597881
jamesjutt <jamesrjutt@gmail.com>
2023-10-25 00:20:37 -0400

    add button component

--- src/Components/MealItem.jsx
+++ src/Components/MealItem.jsx
@@ -1,4 +1,8 @@
 /* eslint-disable react/prop-types */
+
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+
 const MealItem = ({ meal }) => {
   return (
     <li className="meal-item">
@@ -6,11 +10,13 @@ const MealItem = ({ meal }) => {
         <img src={`http://localhost:3000/${meal.image}`} />
         <div>
           <h3>{meal.item}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
---
+++ src/Components/UI/Button.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const Button = ({ children, textOnly, className, ...props }) => {
+  let cssClasses = textOnly ? `text-button` : "button";
+  cssClasses += " " + className;
+  return (
+    <button className={cssClasses} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
